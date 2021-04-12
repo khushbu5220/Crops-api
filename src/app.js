8c154b063c3b05f0b7ac37b95fb9ee875b6c310e
Khushbu <singhkhushbu210@gmail.com>
2021-04-12 23:29:56 +0530

    add routers and controllers

--- src/app.js
+++ src/app.js
@@ -1,63 +1,16 @@
 // nodemon app.js 
 
-const { Router } = require("express");
-const express = require("express");
-require("./db/conn");
-const crop = require("./models/crop");
-
-
+const express = require('express')
 const app = express();
-const port = process.env.PORT || 8000;
 
+
+const { Router } = require("express");
+const cropRouter = require("./routers/crop");
 
 app.use(express.json());
+app.use(cropRouter);
 
-// create a new crops 
-app.post("/crop", async (req,res) => {
-    try{
-        const data = await crop.find();
-
-        res.status(200).json(data);
-
-    } catch (error) {
-        res.status(500).json({ error : error.message})
-    }
-})
-
-// read the data 
-app.get("/crops", async (req,res) => {
-    try{
-        const data = await crop.find();
-
-        res.status(200).send({data});
-
-    } catch (error) {
-        res.status(500).send({ error : error.message})
-    }
-})
-
-
-// get individual data 
-app.get("/crops/:name", async (req,res) => {
-    try{
-        const name = req.params.name;
-        const data = await crop.findOne({name});
-     
-       
-               if(!data){
-                   return res.status(404).send(e);
-               }else{
-                   res.status(200).send(data);
-               }
-
-    }catch(e){
-        res.status(500).send(e);
-    }
-})
-
-
-
-
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`connection is setup at ${port}`);
 })
